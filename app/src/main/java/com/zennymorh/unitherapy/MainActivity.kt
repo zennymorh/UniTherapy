9f62e91b4ee0d92c759eb85eb659f9e96d02607e
Zainab Jimoh <jimohzainab4753@gmail.com>
2021-06-15 20:24:43 +0100

    Rearranged some layouts. BUGS: Scrolling on main screen buggy, Navigation drawer not working

--- app/src/main/java/com/zennymorh/unitherapy/MainActivity.kt
+++ app/src/main/java/com/zennymorh/unitherapy/MainActivity.kt
@@ -3,112 +3,67 @@ package com.zennymorh.unitherapy
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    lateinit var bottomNav: BottomNavigationView
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var actionBarToggle: ActionBarDrawerToggle
-    private lateinit var navigationView: NavigationView
-
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        auth = Firebase.auth
-
-        drawerLayout = findViewById(R.id.drawerLayout)
-
-        actionBarToggle = ActionBarDrawerToggle(this, drawerLayout, mainActivityToolbar,0, 0)
-        drawerLayout.addDrawerListener(actionBarToggle)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        actionBarToggle.syncState()
         setSupportActionBar(mainActivityToolbar)
+        bottomNav = bottom_nav_view
 
-//        navigationView.setNavigationItemSelectedListener(this)
+        navController = findNavController(R.id.nav_host_fragment)
+        setupBottomNavigation()
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        findViewById<NavigationView>(R.id.navigationView)
-            .setupWithNavController(navController)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_therapist,
-                R.id.navigation_community,
-                R.id.navigation_notifications
-            ), drawerLayout
-        )
-        mainActivityToolbar.setupWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
-//        navigationView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                R.id.profile -> {
-//                    Toast.makeText(
-//                        this,
-//                        "Profile clicked",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    // Navigate to the profile screen
-//                    true
-//                }
-//
-//                R.id.signOut -> {
-//                    Toast.makeText(this, "Signing out", Toast.LENGTH_SHORT).show()
-////                    auth.signOut()
-//                    true
-//                }
-//                else -> {
-//                    false
-//                }
-//            }
-//        }
+
+        drawerLayout = drawer_Layout
+        // For Navigation UP
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        //NavigationUI.setupWithNavController(navigation_view,navController)
+        NavigationUI.setupWithNavController(navView, navController)
+
+        navView.setNavigationItemSelectedListener(this)
     }
-
-
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        drawerLayout.openDrawer(navigationView)
+        //return navController.navigateUp()
         return NavigationUI.navigateUp(navController, appBarConfiguration)
+
     }
 
-    override fun onBackPressed() {
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+    private fun setupBottomNavigation() {
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.profile -> Toast.makeText(this, "Clicked item one", Toast.LENGTH_SHORT).show()
-            R.id.signOut -> Toast.makeText(this, "Clicked item two", Toast.LENGTH_SHORT).show()
+        return when (item.itemId) {
+            R.id.profile -> {
+                Toast.makeText(
+                    this,
+                    "Profile clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+                // Navigate to the profile screen
+                true
+            }
+            else -> false
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
