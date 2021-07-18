256b61285b1127ec82806e691c301c47097c4cb4
Zainab Jimoh <jimohzainab4753@gmail.com>
2021-07-18 16:27:28 +0100

    SignOUt Implemented

--- app/src/main/java/com/zennymorh/unitherapy/MainActivity.kt
+++ app/src/main/java/com/zennymorh/unitherapy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.zennymorh.unitherapy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
@@ -12,6 +13,10 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.zennymorh.unitherapy.auth.AuthActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -19,20 +24,21 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var auth: FirebaseAuth
     lateinit var bottomNav: BottomNavigationView
     lateinit var navController: NavController
     lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(mainActivityToolbar)
         bottomNav = bottom_nav_view
 
         navController = findNavController(R.id.nav_host_fragment)
         setupBottomNavigation()
-
-
 
         drawerLayout = drawer_Layout
         // For Navigation UP
@@ -64,6 +70,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     Toast.LENGTH_SHORT
                 ).show()
                 // Navigate to the profile screen
+                true
+            }
+            R.id.signOut -> {
+                Toast.makeText(
+                    this,
+                    "Successfully signed out",
+                    Toast.LENGTH_SHORT
+                ).show()
+                auth.signOut()
+                auth.addAuthStateListener {
+                    if (auth.currentUser == null){
+                        val intent = Intent(this, AuthActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(intent)
+                    }
+                }
                 true
             }
             else -> false
