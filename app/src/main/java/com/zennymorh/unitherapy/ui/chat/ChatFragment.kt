01b56e7b6415a79dc8e546b6381f4e68fbe7085e
Zainab Jimoh <jimohzainab4753@gmail.com>
2021-09-02 09:11:47 +0100

    Working on Chat section

--- app/src/main/java/com/zennymorh/unitherapy/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/zennymorh/unitherapy/ui/chat/ChatFragment.kt
@@ -1,14 +1,32 @@
 package com.zennymorh.unitherapy.ui.chat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import com.zennymorh.unitherapy.R
+import com.zennymorh.unitherapy.auth.AuthActivity
+import com.zennymorh.unitherapy.model.Message
+import kotlinx.android.synthetic.main.fragment_chat.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatFragment : Fragment() {
 
+    val auth = FirebaseAuth.getInstance()
+    val user = auth.currentUser
+    val firestore = FirebaseFirestore.getInstance()
+
+    val chatMessages = ArrayList<Message>()
+    var chatRegistration: ListenerRegistration? = null
+    var roomId: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +38,87 @@ class ChatFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkUser()
+
+        initList()
+        setViewListeners()
+
     }
+
+    private fun setViewListeners() {
+        button_send.setOnClickListener {
+            sendChatMessage()
+        }
+    }
+
+    private fun initList() {
+        if (user == null)
+            return
+
+        recycler_view_messages.layoutManager = LinearLayoutManager(requireActivity())
+        val adapter = ChatAdapter(chatMessages, user.uid)
+        recycler_view_messages.adapter = adapter
+        listenForChatMessages()
+    }
+
+    private fun listenForChatMessages() {
+        roomId = arguments?.getString("roomId")
+        if (roomId == null) {
+            activity?.finish()
+            return
+        }
+
+        chatRegistration = firestore.collection("rooms")
+            .document(roomId!!)
+            .collection("messages")
+            .addSnapshotListener { messageSnapshot, _ ->
+
+                if (messageSnapshot == null || messageSnapshot.isEmpty)
+                    return@addSnapshotListener
+
+                chatMessages.clear()
+
+                for (messageDocument in messageSnapshot.documents) {
+                    chatMessages.add(
+                        Message(
+                            messageDocument["text"] as String,
+                            messageDocument["user"] as String
+                        )
+                    )
+                }
+
+//                chatMessages.sortBy { it.timestamp }
+                recycler_view_messages.adapter?.notifyDataSetChanged()
+            }
+    }
+
+    private fun sendChatMessage() {
+        val message = editText_message.text.toString()
+        editText_message.setText("")
+
+        firestore.collection("rooms").document(roomId!!).collection("messages")
+            .add(mapOf(
+                Pair("text", message),
+                Pair("user", user?.uid)
+//                Pair("timestamp", Timestamp.now())
+            ))
+    }
+
+    private fun checkUser() {
+        if (user == null)
+            launchLogin()
+    }
+
+    private fun launchLogin() {
+        val intent = Intent(requireActivity(), AuthActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        activity?.finish()
+    }
+
+    override fun onDestroy() {
+        chatRegistration?.remove()
+        super.onDestroy()
+    }
+
 }
